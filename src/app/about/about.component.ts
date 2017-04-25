3e96cfcf034d75f5866461537a11ebfcdcb416c9
J2D2Development <joedriscoll79@gmail.com>
2017-04-24 18:20:58 -0700

    More work on about section, fixed projects cards styling

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  firstjQuerySnippet: string = `$(document).ready(function() {<br />
+  &nbsp;&nbsp;&nbsp;&nbsp;$('#userid').focus();<br />
+  });`;
   badCodeSnippet: string = `print '{"enabled": "' . $result_profiles['profiles_enabled'] . '", "position": "' . $result_profiles['profile_position'] . '"}'; `;
 
   constructor() { }
